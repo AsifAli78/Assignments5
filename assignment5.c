b44f447d186f26e4e64e4e425b97fbecbf129b9a
Asif Ali <148493765+AsifAli78@users.noreply.github.com>
2024-10-02 04:03:11 +0500

    Update assignment5.c

--- assignment5.c
+++ assignment5.c
@@ -1,37 +1,29 @@
 #include <stdio.h>
 
+// Function to calculate factorial
+double factorial(int num) {
+    double fact = 1;
+    for (int i = 1; i <= num; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
 int main() {
-    int num1, num2, num3, num4;
-    int sum, smallest, largest;
-    float average;
+    int n;
+    double e = 1.0;
 
-    // Input four integers
-    printf("Enter four integers: ");
-    scanf("%d %d %d %d", &num1, &num2, &num3, &num4);
+    // Input the integer n
+    printf("Enter n = ");
+    scanf("%d", &n);
 
-    // Calculate sum
-    sum = num1 + num2 + num3 + num4;
+    // Calculate the value of e using the series
+    for (int i = 1; i <= n; i++) {
+        e += 1.0 / factorial(i);
+    }
 
-    // Calculate average
-    average = sum / 4.0;
-
-    // Find smallest
-    smallest = num1;
-    if (num2 < smallest) smallest = num2;
-    if (num3 < smallest) smallest = num3;
-    if (num4 < smallest) smallest = num4;
-
-    // Find largest
-    largest = num1;
-    if (num2 > largest) largest = num2;
-    if (num3 > largest) largest = num3;
-    if (num4 > largest) largest = num4;
-
-    // Output results
-    printf("Sum is %d\n", sum);
-    printf("Average is %.2f\n", average);
-    printf("Smallest is %d\n", smallest);
-    printf("Largest is %d\n", largest);
+    // Output the result
+    printf("e = %.6f\n", e);
 
     return 0;
 }
